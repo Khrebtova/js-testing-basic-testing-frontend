854ac379dade331b35aef9a4eb23251bc9f57eab
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-12-05 15:41:01 -0800

    add integration tests

--- src/tests/numbers.test.js
+++ src/tests/numbers.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from 'vitest';
-import { transformToNumber } from '../util/numbers';
+import { transformToNumber, cleanNumbers } from '../util/numbers';
 
 describe('transformToNumber()', () => {
   it('should transform a string to a number', () => {
@@ -33,4 +33,27 @@ describe('transformToNumber()', () => {
   //     expect(result).toThrow();
   // });
 
+});
+
+describe('cleanNumbers()', () => {
+  it('should return an array of number values if an array of string numbers is provided', () => {
+    const input = ['1', '2', '3'];
+    const result = cleanNumbers(input);
+    expect(result[0]).toBeTypeOf('number');
+    // expect(result).toEqual([1, 2, 3]);
+  });
+
+  it('should throw an error if an array with at least one empty string is provided', () => {
+    const input = ['1', '', '3'];
+    const result = () => cleanNumbers(input);
+    expect(result).toThrow();
+  });
+
+  it('should throw an error if an array with at least one non-number string is provided', () => {
+    const input = ['1', 'a', '3'];
+    const result = () => cleanNumbers(input);
+    expect(result).toThrow();
+  });
+
+  
 });
